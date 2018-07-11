54063e81723e992ebfc707404225ec28481e320e
Jerry <jerry.zhang@oocl.com>
2018-07-11 21:35:28 +0800

    myMap finished

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -46,6 +46,7 @@ public class MyMap {
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+//        throw new NotImplementedException();
+        return array.stream().sorted().collect(Collectors.toList());
     }
 }
