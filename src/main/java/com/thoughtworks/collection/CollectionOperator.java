3155053d0a1d9efaa8944a07b2cbe629b092e1b6
Jerry <jerry.zhang@oocl.com>
2018-07-11 20:38:44 +0800

    operator1 finished

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,11 +2,19 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.lang.reflect.Array;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+//        throw new NotImplementedException();
+        int small = left<right?left:right;
+        int big = small==left?right:left;
+        List<Integer> list = IntStream.range(small,big+1).boxed().collect(Collectors.toList());
+        return small==left?list:list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
